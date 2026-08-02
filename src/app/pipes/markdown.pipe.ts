cc47894b8c726e33b555c2b6e027601b907cc641
SebitasDown <areizasebas31@gmail.com>
2026-08-02 13:28:10 -0500

    perf: lazy load heavy markdown libraries in MarkdownPipe to reduce main bundle size

--- src/app/pipes/markdown.pipe.ts
+++ src/app/pipes/markdown.pipe.ts
@@ -1,44 +1,74 @@
-import { Pipe, PipeTransform, inject } from '@angular/core';
+import { Pipe, PipeTransform, inject, ChangeDetectorRef } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { marked } from 'marked';
-import DOMPurify from 'dompurify';
-import katex from 'katex';
 
-@Pipe({ name: 'markdown', standalone: true })
+@Pipe({ name: 'markdown', standalone: true, pure: false })
 export class MarkdownPipe implements PipeTransform {
   private sanitizer = inject(DomSanitizer);
+  private cdr = inject(ChangeDetectorRef);
+  
+  private lastValue = '';
+  private cachedHtml: SafeHtml = '';
+  private loaded = false;
+
+  private marked: any;
+  private dompurify: any;
+  private katex: any;
+
+  constructor() {
+    this.loadDependencies();
+  }
+
+  async loadDependencies() {
+    const [markedLib, dompurifyLib, katexLib] = await Promise.all([
+      import('marked'),
+      import('dompurify'),
+      import('katex')
+    ]);
+    this.marked = markedLib.marked;
+    this.dompurify = dompurifyLib.default || dompurifyLib;
+    this.katex = katexLib.default || katexLib;
+    this.loaded = true;
+    this.cdr.markForCheck();
+  }
 
   transform(value: string): SafeHtml {
     if (!value) return '';
+    if (this.lastValue === value && this.cachedHtml) return this.cachedHtml;
+    
+    this.lastValue = value;
+
+    if (!this.loaded) {
+      // Return unformatted text while loading
+      return this.sanitizer.bypassSecurityTrustHtml(value);
+    }
 
     // 1. Pre-process math blocks (katex)
-    // Block math: $$ ... $$ or \[ ... \]
     let processedValue = value.replace(/\$\$(.*?)\$\$|\\\[(.*?)\\\]/gs, (match, p1, p2) => {
       try {
-        return katex.renderToString(p1 || p2, { displayMode: true, throwOnError: false });
+        return this.katex.renderToString(p1 || p2, { displayMode: true, throwOnError: false });
       } catch (e) {
         return match;
       }
     });
 
-    // Inline math: $ ... $ or \( ... \)
     processedValue = processedValue.replace(/\$(.*?)\$|\\\((.*?)\\\)/g, (match, p1, p2) => {
       try {
-        return katex.renderToString(p1 || p2, { displayMode: false, throwOnError: false });
+        return this.katex.renderToString(p1 || p2, { displayMode: false, throwOnError: false });
       } catch (e) {
         return match;
       }
     });
 
     // 2. Parse markdown
-    const html = marked.parse(processedValue, { async: false }) as string;
+    const html = this.marked.parse(processedValue, { async: false }) as string;
 
-    // 3. Purify HTML (allowing katex classes and mathml)
-    const cleanHtml = DOMPurify.sanitize(html, {
+    // 3. Purify HTML
+    const cleanHtml = this.dompurify.sanitize(html, {
       ADD_TAGS: ['math', 'semantics', 'mrow', 'mi', 'mn', 'mo', 'mspace', 'msqrt', 'mfrac', 'mroot', 'mstyle', 'merror', 'mpadded', 'mphantom', 'mfenced', 'msubsup', 'msup', 'msub', 'mmultiscripts', 'mover', 'munder', 'munderover', 'annotation', 'table', 'tbody', 'thead', 'tr', 'th', 'td'],
       ADD_ATTR: ['display', 'xmlns', 'class', 'style', 'aria-hidden']
     });
 
-    return this.sanitizer.bypassSecurityTrustHtml(cleanHtml);
+    this.cachedHtml = this.sanitizer.bypassSecurityTrustHtml(cleanHtml);
+    return this.cachedHtml;
   }
 }
