6bff964283705c736484a7729a5bbd9550f0b7ec
SebitasDown <areizasebas31@gmail.com>
2026-08-02 13:54:09 -0500

    fix: remove missing environment import in StudyTimerService

--- src/app/services/study-timer.service.ts
+++ src/app/services/study-timer.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 export interface StudySessionRequest {
   subjectId?: number;
